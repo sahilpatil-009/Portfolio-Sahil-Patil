81b19b6a0351cd2dffb35ea391ff76c51ab3a9d1
Sahil Patil <122969868+sahilpatil-009@users.noreply.github.com>
2024-06-19 21:01:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 //Typing Effect
 var typed = new Typed(".text", {
-    strings: ["Java Developer", "Frontend Developer", "Backend Developer" ,"Full-Stack Developer", ],
+    strings: ["Java Developer", "Full-Stack Developer", " AWS & DevOps" ],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
